e6a39718dcc8126a25c424656e8488fe272fc18e
B <B>
2021-03-07 00:46:35 +0800

    add https://www.cnblogs.com/u128393/p/11629969.html

--- SHiptv.sh
+++ SHiptv.sh
@@ -22,7 +22,7 @@ echo 正在删除旧文件
 rm -rf wan-start*
 #下载自启动脚本
 echo 正在下载新文件
-wget -q --no-check-certificate https://raw.githubusercontent.com/PatrikYang/SH-4K-IPTV-Merlin/master/wan-start
+wget -q --no-check-certificate https://raw.githubusercontent.com/bk1d/SH-4K-IPTV-Merlin/master/wan-start
 #设置权限
 chmod -R 0755 wan-start
 #完成提示
@@ -42,7 +42,7 @@ echo 正在删除旧文件
 rm -rf iptv4k-dns.conf
 #下载开机运行脚本
 echo 正在下载新文件
-wget -q --no-check-certificate https://raw.githubusercontent.com/PatrikYang/SH-4K-IPTV-Merlin/master/iptv4k-dns.conf
+wget -q --no-check-certificate https://raw.githubusercontent.com/bk1d/SH-4K-IPTV-Merlin/master/iptv4k-dns.conf
 #设置权限
 chmod -R 0644 iptv4k-dns.conf
 #完成提示
